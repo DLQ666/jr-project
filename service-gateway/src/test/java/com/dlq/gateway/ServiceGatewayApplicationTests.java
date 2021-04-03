25964ec0ac2dd59f2032de364404359a42560dfa
D·Teach <1114626450@qq.com>
2021-04-03 20:23:51 +0800

    添加微服务网关Gateway

---
+++ service-gateway/src/test/java/com/dlq/gateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlq.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
