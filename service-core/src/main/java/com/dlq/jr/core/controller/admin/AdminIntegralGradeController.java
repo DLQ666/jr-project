21fc883b17134413bbda5c9fc99a9843b86033a6
D·Teach <1114626450@qq.com>
2021-04-03 20:42:28 +0800

    简单示例Swagger2注解

--- service-core/src/main/java/com/dlq/jr/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/com/dlq/jr/core/controller/admin/AdminIntegralGradeController.java
@@ -2,6 +2,8 @@ package com.dlq.jr.core.controller.admin;
 
 import com.dlq.jr.core.pojo.entity.IntegralGrade;
 import com.dlq.jr.core.service.IntegralGradeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
  *@author: Hasee
  *@create: 2021-04-03 19:43
  */
+@Api(tags = "积分等级管理")
 @RestController
 @RequestMapping("/admin/core/integralGrade")
 public class AdminIntegralGradeController {
@@ -20,11 +23,13 @@ public class AdminIntegralGradeController {
     @Autowired
     private IntegralGradeService integralGradeService;
 
+    @ApiOperation("积分等级列表")
     @GetMapping("/list")
     public List<IntegralGrade> list(){
         return integralGradeService.list();
     }
 
+    @ApiOperation("根据id删除记录")
     @DeleteMapping("/remove/{id}")
     public boolean removeById(@PathVariable("id")Long id){
         return integralGradeService.removeById(id);
