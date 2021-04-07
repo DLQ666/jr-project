dfb6943fcbdf15644678ea8b79e641e33ae01d2c
D·Teach <1114626450@qq.com>
2021-04-07 21:26:03 +0800

    修改网关跨域配置类

--- service-gateway/src/main/java/com/dlq/gateway/config/CorsConfig.java
+++ service-gateway/src/main/java/com/dlq/gateway/config/CorsConfig.java
@@ -19,9 +19,10 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedMethod("*");
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
+        config.setAllowCredentials(true); //是否允许携带cookie
+        config.addAllowedOrigin("*"); //可接受的域，是一个具体域名或者*（代表任意域名）
+        config.addAllowedHeader("*"); //允许携带的头
+        config.addAllowedMethod("*"); //允许访问的方式
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**", config);
