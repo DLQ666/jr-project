4157cc10a3ed2fd951275374634c2807c1165a52
D·Teach <1114626450@qq.com>
2021-04-07 13:31:40 +0800

    添加用户信息常量

--- service-core/src/main/java/com/dlq/jr/core/pojo/entity/UserInfo.java
+++ service-core/src/main/java/com/dlq/jr/core/pojo/entity/UserInfo.java
@@ -24,6 +24,10 @@ import lombok.EqualsAndHashCode;
 @ApiModel(value="UserInfo对象", description="用户基本信息")
 public class UserInfo implements Serializable {
 
+    public static final Integer STATUS_NORMAL = 1;
+    public static final Integer STATUS_LOCKED = 0;
+    public static final String USER_AVATAR = "https://dlq-jr.oss-cn-beijing.aliyuncs.com/avatar/default.jpg";
+
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "编号")
