d2cc7b7d711e0875b042f8bfa6788444b2c7badf
D·Teach <1114626450@qq.com>
2021-04-07 13:42:57 +0800

    优化用户注册参数校验

--- service-core/src/main/java/com/dlq/jr/core/controller/api/UserInfoController.java
+++ service-core/src/main/java/com/dlq/jr/core/controller/api/UserInfoController.java
@@ -4,6 +4,7 @@ package com.dlq.jr.core.controller.api;
 import com.dlq.jr.common.exception.Assert;
 import com.dlq.jr.common.result.R;
 import com.dlq.jr.common.result.ResponseEnum;
+import com.dlq.jr.common.util.RegexValidateUtils;
 import com.dlq.jr.core.pojo.vo.RegisterVo;
 import com.dlq.jr.core.service.UserInfoService;
 import io.swagger.annotations.Api;
@@ -43,10 +44,19 @@ public class UserInfoController {
 
         //校验验证码是否正确
         String mobile = registerVo.getMobile();
+        String password = registerVo.getPassword();
         String code = registerVo.getCode();
+        //进行合法校验
+        Assert.notEmpty(mobile,ResponseEnum.MOBILE_NULL_ERROR);
+        Assert.notEmpty(password, ResponseEnum.PASSWORD_NULL_ERROR);
+        Assert.notEmpty(code, ResponseEnum.CODE_NULL_ERROR);
+        Assert.isTrue(RegexValidateUtils.checkCellphone(mobile), ResponseEnum.MOBILE_ERROR);
+
+        //获取redis中验证码
         String codeGen = (String) redisTemplate.opsForValue().get("jr:sms:code:" + mobile);
         //String codeMobile = (String) redisTemplate.opsForValue().get("jr:sms:code:" + mobile);
         //上边这种用泛型的不行 取出数据为  "\"1234\"" -----> 串中串
+        //进行对比
         Assert.equals(code, codeGen, ResponseEnum.CODE_ERROR);
 
         //注册
