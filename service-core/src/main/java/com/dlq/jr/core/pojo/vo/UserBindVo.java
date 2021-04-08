96d2c2d8288347ea9bc3348affa1f7f88341011f
D·Teach <1114626450@qq.com>
2021-04-08 14:12:07 +0800

    添加用户绑定实体vo/状态枚举类

---
+++ service-core/src/main/java/com/dlq/jr/core/pojo/vo/UserBindVo.java
@@ -0,0 +1,31 @@
+package com.dlq.jr.core.pojo.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ *@program: jr-project
+ *@description:
+ *@author: Hasee
+ *@create: 2021-04-08 14:08
+ */
+@Data
+@ApiModel(description = "账户绑定")
+public class UserBindVo {
+
+    @ApiModelProperty(value = "身份证号")
+    private String idCard;
+
+    @ApiModelProperty(value = "用户姓名")
+    private String name;
+
+    @ApiModelProperty(value = "银行类型")
+    private String bankType;
+
+    @ApiModelProperty(value = "银行卡号")
+    private String bankNo;
+
+    @ApiModelProperty(value = "手机号")
+    private String mobile;
+}
