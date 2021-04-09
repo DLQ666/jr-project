01b082e05184baa3ab03f5f91e9164980f307d90
D·Teach <1114626450@qq.com>
2021-04-09 11:30:32 +0800

    添加根据dictCode和value查询数据字典名称接口

--- service-core/src/main/java/com/dlq/jr/core/service/impl/DictServiceImpl.java
+++ service-core/src/main/java/com/dlq/jr/core/service/impl/DictServiceImpl.java
@@ -103,6 +103,23 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
         return this.listByParentId(dict.getId());
     }
 
+    @Override
+    public String getNameByParentDictCodeAndValue(String dictCode, Integer value) {
+        QueryWrapper<Dict> dictQueryWrapper = new QueryWrapper<>();
+        dictQueryWrapper.eq("dict_code", dictCode);
+        Dict parentDict = baseMapper.selectOne(dictQueryWrapper);
+        if (parentDict == null) {
+            return "";
+        }
+        dictQueryWrapper = new QueryWrapper<>();
+        dictQueryWrapper.eq("parent_id", parentDict.getId()).eq("value", value);
+        Dict dict = baseMapper.selectOne(dictQueryWrapper);
+        if (dict == null) {
+            return "";
+        }
+        return dict.getName();
+    }
+
     private boolean hasChildren(Long id){
         QueryWrapper<Dict> dictQueryWrapper = new QueryWrapper<>();
         dictQueryWrapper.eq("parent_id", id);
